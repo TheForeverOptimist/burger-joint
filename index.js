531396af8a61ab5294eaa037a337c1ca3760a508
TheForeverOptimist <sean@elevate.app>
2024-05-17 21:35:48 -0700

    updated selection items css

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const createItem = (items) => {
         <div class="emoji">${item.emoji}</div>
         <div class="list">
             <h3>${item.name}</h3>
-            <p>${item.ingredients}</p>
+            <p class="ingredients">${item.ingredients}</p>
             <p>$${item.price}</p>
         </div>
-        <i class="fa-solid fa-circle-plus"></i>
+        <i class="fa-solid fa-circle-plus" data-add></i>
         </div>
         <hr />
         
@@ -26,3 +26,7 @@ const createItem = (items) => {
 }
 
 createItem(menuArray);
+
+document.addEventListener("click", function(e){
+    e.target.dataset.add
+})
