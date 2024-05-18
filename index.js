fb1ff8451adca4538407e6651dcf97234c3fdfae
TheForeverOptimist <sean@elevate.app>
2024-05-17 21:13:22 -0700

    added all selection elements

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const selections = document.getElementById("selection")
 const createItem = (items) => {
     console.log(items)
     items.forEach(function(item){
-        selections.innerHTML += 
-        `<div class="emoji">${item.emoji}</div>
+        selections.innerHTML += `
+        <div class="menuItems">
+        <div class="emoji">${item.emoji}</div>
+        <div class="list">
+            <h3>${item.name}</h3>
+            <p>${item.ingredients}</p>
+            <p>$${item.price}</p>
+        </div>
+        <i class="fa-solid fa-circle-plus"></i>
+        </div>
         <hr />
         
         
         
         
-        `
+        `;
     })
 }
 
