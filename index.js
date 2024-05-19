d15164155c38a0c0170ab596277ec5d16ee4c11e
TheForeverOptimist <sean@elevate.app>
2024-05-18 18:33:25 -0700

    updating ordering ui

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {menuArray} from './data.js'
 
 const selections = document.getElementById("selection")
+let total = 0;
 
 const createItem = (items) => {
     selections.innerHTML = '';
@@ -11,10 +12,10 @@ const createItem = (items) => {
         <div class="emoji">${item.emoji}</div>
         <div class="list">
             <h3>${item.name}</h3>
-            <p class="ingredients">${item.ingredients}</p>
+            <p class="ingredients">${item.ingredients.join(', ')}</p>
             <p>$${item.price}</p>
         </div>
-        <i class="fa-solid fa-circle-plus" data-add></i>
+        <i class="fa-solid fa-circle-plus" data-add="${item.id}"></i>
         </div>
         <hr />
         
@@ -27,6 +28,32 @@ const createItem = (items) => {
 
 createItem(menuArray);
 
+const newItem = document.getElementById("yourOrder")
+
 document.addEventListener("click", function(e){
-    e.target.dataset.add
+    if(e.target.dataset.add){
+        const itemId = e.target.dataset.add
+        const addedItem = menuArray.find(item => item.id == itemId)
+        if(addedItem){
+            addOrderItem(addedItem)
+            updateTotal(addedItem.price)
+        }
+    }
 })
+
+const addOrderItem = (addedItem) => {
+    newItem.innerHTML += `
+        <h4>Your order</h4>
+        <div class="item-line">
+            <h5>${addedItem.name}</h5>
+            <button class="removeBtn">remove</button>
+            <p>${addedItem.price}</p>
+        </div>
+    
+    
+    `
+}
+
+const updateTotal = (price) => {
+    total += price;
+}
